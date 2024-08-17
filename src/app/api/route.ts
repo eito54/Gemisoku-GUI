98e6cdca598f8746d3e44f00993e796fa95ffa55
keisuke071411 <i.love.football0714@gmail.com>
2024-08-17 19:45:55 +0900

    ✨ 画像を文字起こしする機能を作成

---
+++ src/app/api/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+import { googleVisionClient } from "../infra/gcp";
+
+
+export async function POST(req: NextRequest) {
+  const json = await req.json();
+
+  const [result] = await googleVisionClient.textDetection(json.imageUrl);
+
+  return NextResponse.json({ result });
+}
