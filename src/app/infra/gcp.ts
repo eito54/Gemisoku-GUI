5bf23e7b5ff9ffae4c53e35c896bf9c4fa66ec83
keisuke071411 <i.love.football0714@gmail.com>
2024-09-19 21:54:13 +0900

    🔥 不要のファイルを削除

--- src/app/infra/gcp.ts
+++
@@ -1,6 +0,0 @@
-import googleVisionApi from "@google-cloud/vision";
-import file from "../credientials/mk8dx-bot-463461693972.json";
-
-export const googleVisionClient = new googleVisionApi.ImageAnnotatorClient({
-  credentials: file,
-});
