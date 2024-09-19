3bc7543612af1049f481afe05c18076686203973
keisuke071411 <i.love.football0714@gmail.com>
2024-09-19 23:20:13 +0900

    ✨ Form Componentを作成

---
+++ src/app/_components/Form/index.tsx
@@ -0,0 +1,89 @@
+import { Input } from "@/components/ui/input";
+import type { TeamScore } from "@/types";
+import { type Dispatch, type SetStateAction, useEffect, useRef } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
+
+type FormProps = {
+  teamScoreList: TeamScore[];
+  setTeamScoreList: Dispatch<SetStateAction<TeamScore[]>>;
+};
+
+type FormValues = {
+  results: TeamScore[];
+};
+
+export const Form = ({ teamScoreList, setTeamScoreList }: FormProps) => {
+  const { register, handleSubmit, control, reset, watch } = useForm<FormValues>(
+    {
+      defaultValues: {
+        results: teamScoreList.sort((a, b) => b.score - a.score),
+      },
+    },
+  );
+
+  const { fields, remove } = useFieldArray({
+    control,
+    name: "results",
+  });
+
+  // 外部のteamScoreList更新に対する反映フラグ
+  const isUpdatingFromTeamScoreList = useRef(false);
+
+  // teamScoreListが変更された時にフォームをリセットして更新
+  useEffect(() => {
+    if (!isUpdatingFromTeamScoreList.current) {
+      reset({
+        results: teamScoreList.sort((a, b) => b.score - a.score),
+      });
+    }
+    isUpdatingFromTeamScoreList.current = false;
+  }, [teamScoreList, reset]);
+
+  // フォームの値を監視して、teamScoreListに反映する
+  const watchedFields = watch("results");
+
+  // labelが空になったフィールドを削除
+  useEffect(() => {
+    watchedFields.forEach((field, index) => {
+      if (field.team === "") {
+        remove(index); // labelが空ならフィールドを削除
+      }
+    });
+
+    if (JSON.stringify(watchedFields) !== JSON.stringify(teamScoreList)) {
+      setTeamScoreList(watchedFields);
+      isUpdatingFromTeamScoreList.current = true; // teamScoreList更新由来でのリセットを避ける
+    }
+  }, [watchedFields, teamScoreList, setTeamScoreList, remove]);
+
+  const onSubmit = (data: FormValues) => {
+    console.info("フォーム送信:", data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-col gap-2 mb-10">
+        {fields.map((item, index) => (
+          <div
+            key={item.id}
+            className="h-16 flex items-center gap-0.5 text-white"
+          >
+            <Input
+              {...register(`results.${index}.team`)}
+              defaultValue={item.team}
+              className="w-24 h-full text-2xl rounded-l-lg text-center bg-primary"
+            />
+            <Input
+              {...register(`results.${index}.score`, {
+                valueAsNumber: true, // 入力を数値として扱う
+              })}
+              defaultValue={item.score}
+              type="number"
+              className="w-24 h-full text-2xl rounded-r-lg text-center bg-primary"
+            />
+          </div>
+        ))}
+      </div>
+    </form>
+  );
+};
