e7b3933d37897b31f9b583f1dd1ef5c9d4f91caf
keisuke071411 <i.love.football0714@gmail.com>
2024-09-19 22:36:56 +0900

    🏷️ ReceResult, TeamScore, UploadImageToChatGptResponse型を作成

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export type RaceResult = {
+  rank: number;
+  name: string;
+  team: string;
+  score: number;
+};
+
+export type TeamScore = {
+  team: string;
+  score: number;
+};
+
+export type UploadImageToChatGptResponse = {
+  raceResults: RaceResult[];
+};
