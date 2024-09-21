4571af93822131ea82d8c8ff99d1a33ce7b4dbe2
keisuke071411 <i.love.football0714@gmail.com>
2024-09-21 19:43:56 +0900

    ✨ 順位が入れ替わった時のアニメーションを付与

--- src/app/_components/Form/index.tsx
+++ src/app/_components/Form/index.tsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components/ui/input";
 import type { TeamScore } from "@/types";
+import { AnimatePresence, Reorder } from "framer-motion";
 import { Diff } from "lucide-react";
 import { type Dispatch, type SetStateAction, useEffect, useRef } from "react";
 import { useFieldArray, useForm } from "react-hook-form";
@@ -14,13 +15,14 @@ type FormValues = {
 };
 
 export const Form = ({ teamScoreList, setTeamScoreList }: FormProps) => {
-  const { register, handleSubmit, control, reset } = useForm<FormValues>({
-    defaultValues: {
-      results: teamScoreList.sort((a, b) => b.score - a.score),
-    },
-  });
+  const { register, handleSubmit, control, reset, setValue } =
+    useForm<FormValues>({
+      defaultValues: {
+        results: teamScoreList.sort((a, b) => b.score - a.score),
+      },
+    });
 
-  const { fields, remove } = useFieldArray({
+  const { fields } = useFieldArray({
     control,
     name: "results",
   });
@@ -45,49 +47,63 @@ export const Form = ({ teamScoreList, setTeamScoreList }: FormProps) => {
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="pr-20">
       <div className="w-fit ml-auto flex flex-col gap-2">
-        {fields.map((item, index) => (
-          <div
-            key={item.id}
-            className="h-16 flex items-center gap-2 text-white relative"
+        <AnimatePresence>
+          <Reorder.Group
+            axis="y"
+            values={fields}
+            onReorder={(newOrder) => setValue("results", newOrder)}
           >
-            <Input
-              {...register(`results.${index}.team`, {
-                onChange: (e) => {
-                  // labelが空ならフィールドを削除
-                  if (e.target.value === "") {
-                    remove(index);
-                  }
-                },
-              })}
-              defaultValue={item.team}
-              className="w-24 h-full text-2xl rounded-l-lg text-center bg-primary [appearance:textfield] [&::-webkit-outer-spin-button] [&::-webkit-inner-spin-button]"
-            />
-            <Input
-              {...register(`results.${index}.score`, {
-                valueAsNumber: true, // 入力を数値として扱う
-                // scoreが変更された時にteamScoreListを更新
-                onBlur: (e) => {
-                  setTeamScoreList((prev) =>
-                    prev.map((item) =>
-                      item.team === fields[index].team
-                        ? { ...item, score: e.target.valueAsNumber }
-                        : item,
-                    ),
-                  );
-                },
-              })}
-              defaultValue={item.score}
-              type="number"
-              className="w-24 h-full text-2xl rounded-r-lg text-center bg-primary [appearance:textfield] [&::-webkit-outer-spin-button] [&::-webkit-inner-spin-button]"
-            />
-            {fields.length !== index + 1 && (
-              <p className="flex items-center min-w-12 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] text-2xl text-center font-bold self-end absolute -right-16 -bottom-5">
-                <Diff />
-                {fields[index].score - fields[index + 1].score}
-              </p>
-            )}
-          </div>
-        ))}
+            {fields.map((item, index) => (
+              <Reorder.Item key={item.id} value={item.id} drag={false}>
+                <div
+                  key={item.id}
+                  className="h-16 flex items-center gap-2 text-white relative"
+                >
+                  <Input
+                    {...register(`results.${index}.team`, {
+                      onChange: (e) => {
+                        // labelが空ならフィールドを削除
+                        if (e.target.value === "") {
+                          setTeamScoreList((prev) =>
+                            prev.filter(
+                              (teamItem) => teamItem.team !== item.team,
+                            ),
+                          );
+                        }
+                      },
+                    })}
+                    defaultValue={item.team}
+                    className="w-24 h-full text-2xl rounded-l-lg text-center bg-primary [appearance:textfield] [&::-webkit-outer-spin-button] [&::-webkit-inner-spin-button]"
+                  />
+                  <Input
+                    {...register(`results.${index}.score`, {
+                      valueAsNumber: true, // 入力を数値として扱う
+                      // scoreが変更された時にteamScoreListを更新
+                      onBlur: (e) => {
+                        setTeamScoreList((prev) =>
+                          prev.map((teamItem) =>
+                            teamItem.team === fields[index].team
+                              ? { ...teamItem, score: e.target.valueAsNumber }
+                              : teamItem,
+                          ),
+                        );
+                      },
+                    })}
+                    defaultValue={item.score}
+                    type="number"
+                    className="w-24 h-full text-2xl rounded-r-lg text-center bg-primary [appearance:textfield] [&::-webkit-outer-spin-button] [&::-webkit-inner-spin-button]"
+                  />
+                  {fields.length !== index + 1 && (
+                    <p className="flex items-center min-w-12 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] text-2xl text-center font-bold self-end absolute -right-16 -bottom-5">
+                      <Diff />
+                      {fields[index].score - fields[index + 1].score}
+                    </p>
+                  )}
+                </div>
+              </Reorder.Item>
+            ))}
+          </Reorder.Group>
+        </AnimatePresence>
       </div>
     </form>
   );
