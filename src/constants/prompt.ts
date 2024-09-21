946405d53119fb3704ced397774a066697e69fd5
keisuke071411 <i.love.football0714@gmail.com>
2024-09-21 14:00:53 +0900

    🎨 処理速度向上のため、得点は取得しないようにプロンプトを修正

--- src/constants/prompt.ts
+++ src/constants/prompt.ts
@@ -1,6 +1,6 @@
 export const PROMPT = `
   ## instruction ##
-  ゲームの結果画面を解析して全員分の順位とユーザー名、得点とチームを出力してください。
+  ゲームの結果画面を解析して全員分の順位とユーザー名、チームを出力してください。
 
   結果画面には左から以下の項目が書いてあります。
   1. 順位
@@ -14,9 +14,6 @@ export const PROMPT = `
   - [name]はそのまま出力してください。
   - [team]は[name]の最初の一文字目のアルファベットです。
   - [team]は大文字で出力してください。
-  - [score]は得点です。
-  - [score]は正の整数で出力してください。
-
 
   ## Output Format ##
   {
@@ -25,7 +22,6 @@ export const PROMPT = `
         rank: [rank],
         name: [name],
         team: [team],
-        score: [score],
       },
     ]
   }
