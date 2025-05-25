d683af8894c43f543e2b6644ca5f9a2a55aa26e0
eito54 <eito54t@gmail.com>
2025-05-25 23:48:41 +0900

    Remove all existing files

--- src/constants/image.ts
+++
@@ -1,5 +0,0 @@
-export const IMAGE_OPTIONS = {
-  maxSizeMB: 0.3, // 最大ファイルサイズを1MBに設定
-  // maxWidthOrHeight: 800, // 最大幅または高さを800pxに設定
-  useWebWorker: true, // Web Workerを使用して圧縮を非同期化
-};
--- src/infra/openAi.ts
+++
@@ -1,7 +0,0 @@
-import OpenAI from "openai";
-
-export const openAi = new OpenAI({
-  organization: process.env.OPEN_AI_ORGANIZATION,
-  project: process.env.OPEN_AI_PROJECT,
-  apiKey: process.env.OPEN_AI_API_KEY,
-});
